1386d250b487b034ce2488d1bcb158baaf7bcf4f
sell488 <sell488sftoday@gmail.com>
2018-04-30 12:50:10 -0500

    Testing serialization

--- src/com/projectlearn/login/Login.java
+++ src/com/projectlearn/login/Login.java
@@ -11,20 +11,30 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Login extends Application {
     //TODO REORGANIZE PACKAGES
     public  Stage stage;
     public Scene mainAccountScene;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //default account
         AccountList accounts = new AccountList();
         int num = 1234;
         int balance = 10;
         Account stefan = new Account(num, balance, "Stefan", "sell488sftoday@gmail.com", "1234", 12, "Owner");
         accounts.setAccounts(num, stefan);
+
+        File accountsStored = new File("Accounts Stored.txt");
+        FileOutputStream fos = new FileOutputStream(accountsStored);
+        DataOutputStream dos = new DataOutputStream(fos);
+        dos.writeUTF("Accounts Content");
+
+        FileInputStream fis = new FileInputStream(accountsStored);
+        DataInputStream dis = new DataInputStream(fis);
+        String str = dis.readUTF();
+        System.out.println(str);
 
         launch(args);
     }
